c2aa4d25ee00a302d0cdb3453a45dc95051f2536
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-06-26 21:48:22 +0530

    adding mongoDB

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb://localhost/urlShoertner',{
+    useNewUrlParser : true, useUnifiedTopology : true
+})
+mongoose.connection.on('error', (err)=>{
+    console.log('>> Failed to connect to MongoDB, retrying...');
+})
 
 app.set('view engine' , 'ejs')
 
 app.get('/', (req,res) => {
     res.render('index')
+})
+
+app.post('/shortUrls', (req, res) => {
+
 })
 
 app.listen(process.env.PORT || 5000);
