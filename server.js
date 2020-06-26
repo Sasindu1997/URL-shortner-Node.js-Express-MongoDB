6805501493d88596ce0462e53ae064480cbfa4a8
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-06-26 23:36:39 +0530

    adding db models

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const shortUrl = require('./models/shortUrl')
 const app = express()
 
 mongoose.connect('mongodb://localhost/urlShoertner',{
@@ -10,13 +12,15 @@ mongoose.connection.on('error', (err)=>{
 })
 
 app.set('view engine' , 'ejs')
+app.use(express.urlencoded({ extended : false}))
 
 app.get('/', (req,res) => {
     res.render('index')
 })
 
-app.post('/shortUrls', (req, res) => {
-
+app.post('/shortUrls', async(req, res) => {
+    await shortUrl.create({ full : req.body.fullUrl })
+    res.redirect('index')
 })
 
 app.listen(process.env.PORT || 5000);
